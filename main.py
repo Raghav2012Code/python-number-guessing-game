a4945ebae700405377963c835e1735bb75c56d07
Raghav Krishna <raghavgamerz670@gmail.com>
2026-04-16 20:44:08 +0530

    Initial version - number guessing game (one guess only)

--- main.py
+++ main.py
@@ -1,1 +1,18 @@
+import random
+
 print("Welcome to the Number Guessing Game!")
+print("I am thinking of a number between one and one hundred.")
+
+rand_number = random.randint(1, 100)   # generate FIRST
+
+usr_guess = int(input("Guess a number between 1 and 100: "))
+
+if usr_guess == rand_number:
+    print("You have successfully guessed the right number!")
+    print("Good Job!!")
+elif usr_guess > rand_number:
+    print("Too High!")
+    print("Guess Lower.")
+else:
+    print("Too Low!")
+    print("Guess Higher.")
