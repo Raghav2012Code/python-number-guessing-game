08d24263fa62f192d26417627b1776e716a559df
Raghav Krishna <raghavgamerz670@gmail.com>
2026-04-17 20:58:51 +0530

    Add while loop for multiple guesses and attempt counter – game now continues until correct guess, tracks attempts, and offers replay

--- main.py
+++ main.py
@@ -1,18 +1,30 @@
 import random
 
-print("Welcome to the Number Guessing Game!")
-print("I am thinking of a number between one and one hundred.")
 
-rand_number = random.randint(1, 100)   # generate FIRST
 
-usr_guess = int(input("Guess a number between 1 and 100: "))
+while True:
+    print("Welcome to the Number Guessing Game!")
+    print("I am thinking of a number between one and one hundred.")
 
-if usr_guess == rand_number:
-    print("You have successfully guessed the right number!")
-    print("Good Job!!")
-elif usr_guess > rand_number:
-    print("Too High!")
-    print("Guess Lower.")
-else:
-    print("Too Low!")
-    print("Guess Higher.")
+    rand_number = random.randint(1, 100)   # generate FIRST
+    attempts = 0
+    
+    while True:
+        usr_guess = int(input("Guess a number between 1 and 100: "))
+        attempts = attempts + 1 
+
+        if usr_guess == rand_number:
+            print("You have successfully guessed the right number!")
+            print(f"It took you {attempts} to guess the number!")
+            print("Good Job!!")
+            break
+        elif usr_guess > rand_number:
+            print("Too High!")
+            print("Guess Lower.")
+        else:
+            print("Too Low!")
+            print("Guess Higher.")
+
+    play_again = input("Do you want to play again (y/n): ")
+    if play_again != "y":
+        break
